ec177bc0092c09f848b8d72642e3b564219258c4
CChavez27 <136861895+CChavez27@users.noreply.github.com>
2024-05-09 19:42:16 -0600

    Lógica Proposicional

--- 006-Logica proposicional-Encadenamiento_ Hacia Delante y Atrás.py
+++ 006-Logica proposicional-Encadenamiento_ Hacia Delante y Atrás.py
@@ -1,8 +1,5 @@
-#Alma Paola Garcia Landeros  
-#21110038  
-#6E1  
-#Inteligencia Artificial  
-#Centro de Enseñanza Tecnica Industrial 
+#Salazar Chavez Cristian Uriel
+#21310215
 
 class MotorInferencia:
     def __init__(self, base_conocimiento):
