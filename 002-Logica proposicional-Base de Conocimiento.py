64afa853456bd4b43fe32d854da1ba6bc27faed6
CChavez27 <136861895+CChavez27@users.noreply.github.com>
2024-05-09 19:41:10 -0600

    Lógica Proposicional

--- 002-Logica proposicional-Base de Conocimiento.py
+++ 002-Logica proposicional-Base de Conocimiento.py
@@ -1,8 +1,5 @@
-#Alma Paola Garcia Landeros 
-#21110038 
-#6E1 
-#Inteligencia Artificial 
-#Centro de Enseñanza Tecnica Industrial 
+#Salazar Chavez Cristian Uriel
+#21310215
 
 class BaseConocimiento:
     def __init__(self):
